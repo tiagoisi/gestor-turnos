c76689be4cb6bc4e02f0f418090af527b23e648c
Tiago Isidro <tiagoisidromadoery123@gmail.com>
2025-10-07 23:17:47 -0300

    fixes

--- back/src/routes/appointmentsRouter.ts
+++ back/src/routes/appointmentsRouter.ts
@@ -1,8 +1,3 @@
-//? HACER LOS SIGUIENTE METODOS =>
-//* GET /appointments => Obtener todos los turnos
-//* GET /appointments/id: => Obtener un turno por ID
-//* POST /appointments/schedule => Crear un nuevo turno
-
 import { Router } from "express";
 import { cancel, getAllAppointments, getAppointmentById, schedule } from "../controllers/appointmentController";
 import validateAppointment from "../middlewares/validateAppointment";
@@ -15,7 +10,4 @@ appointmentRouter.post("/shedule", validateAppointment, schedule);
 appointmentRouter.put("/cancel/:id", cancel);
 
 export default appointmentRouter;
-
-//! PUT /appointments/cancel => Cancelar un turno
-//? POR AHORA SOLO MANDAR UN MENSAJE 
  
