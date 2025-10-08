701ec0718c13368b65ff4005d924ce8b299b7ed9
Tiago Isidro <tiagoisidromadoery123@gmail.com>
2025-10-07 23:18:01 -0300

    fixes

--- back/src/routes/userRouter.ts
+++ back/src/routes/userRouter.ts
@@ -1,8 +1,3 @@
-//? LAS RUTAS QUE VAMOS A TENER ACA SON =>
-//* GET /users => Obtener todos los usuarios
-//* GET /users/:id => Obtener un usuario por su ID
-//* POST /users/register => Crear un nuevo usuario
-
 import { Router } from "express";
 import { getAllUsers, getUserById, login, register } from "../controllers/userController";
 import validateUser from "../middlewares/validateUser";
