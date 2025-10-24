784d96c018f2c54128a280128f3288f82641ef96
Tiagoisi <tiagoisidromadoery123@gmail.com>
2025-10-24 01:35:25 -0300

    fix: CSS import case sensitivity

--- front/src/components/appoinmentCard/appointmentCard.jsx
+++ front/src/components/appoinmentCard/appointmentCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./AppointmentCard.module.css";
+import styles from "./appointmentCard.module.css";
 
 export default function AppointmentCard({
     id, date, time, status, description, onCancel 
