9ab00ef7de4115935e948845ec29bd39a4d1ed83
Tiagoisi <tiagoisidromadoery123@gmail.com>
2025-10-24 01:33:33 -0300

    fix: CSS import case sensitivity

--- front/src/components/navbar/NavBar.jsx
+++ front/src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import styles from "./Navbar.module.css"
+import styles from "./NavBar.module.css"
 import logo from "../../assets/logo.png"
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
